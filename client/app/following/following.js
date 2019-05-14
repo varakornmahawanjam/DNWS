1412e214a4171a43d76d6084818dbfa5f3e00d29
isne534 <varakorn_mahawanjam@cmu.ac.th>
2019-05-14 11:16:39 +0700

    add follow,unfollow

--- client/app/following/following.js
+++ client/app/following/following.js
@@ -8,7 +8,17 @@ angular.module('followingList', ['ngRoute'])
 
       const requestOptions = {
           headers: { 'X-session': $rootScope.x_session }
-      };
+		};
+		self.sendfollow = function sendfollow(username) {
+
+			const data = "followingname=" + encodeURIComponent(username);
+			$http.post('http://localhost:8080/twitterapi/following/', data, requestOptions);
+		}
+		self.sendunfollow = function sendunfollow(username) {
+			$http.defaults.headers.delete = { 'X-session': $rootScope.x_session };
+			const data = "followingname=" + encodeURIComponent(username);
+			$http.delete('http://localhost:8080/twitterapi/following/?' + data);
+		}
 
       $http.get('http://localhost:8080/twitterapi/following/', requestOptions).then(function (response) {
         self.followings = response.data;
